7ec61993ac57b49e978fc5c0aacabf7f5fc3b693
Hamza-Azeem1 <hamzaazeem023@gmail.com>
2023-08-25 17:28:53 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,98 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import Select from 'react-select';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [amount, setAmount] = useState('');
+  const [fromCurrency, setFromCurrency] = useState('USD');
+  const [toCurrency, setToCurrency] = useState('EUR');
+  const [conversionRate, setConversionRate] = useState(1);
+  const [convertedAmount, setConvertedAmount] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const API_KEY = import.meta.env.VITE_CONVERTER_API_KEY;
+  const API_URL = `http://data.fixer.io/api/latest?access_key=${API_KEY}`;
+
+  const [currencyOptions, setCurrencyOptions] = useState([]);
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then(response => response.json())
+      .then(data => {
+        const rates = data.rates;
+        const options = Object.keys(rates);
+        setCurrencyOptions(options);
+        const rate = rates[toCurrency] / rates[fromCurrency];
+        setConversionRate(rate);
+      });
+  }, [API_URL, fromCurrency, toCurrency]);
+
+  const handleConvert = () => {
+    if (!amount) {
+      setErrorMessage('Please enter an amount.');
+      setConvertedAmount('');
+      return;
+    }
+
+    if (isNaN(amount) || !/^\d+(\.\d{1,2})?$/.test(amount)) {
+      setErrorMessage('Invalid input. Please enter a valid amount.');
+      setConvertedAmount('');
+      return;
+    }
+
+    const convertedValue = (parseFloat(amount) * conversionRate).toFixed(2);
+    setConvertedAmount(convertedValue);
+    setErrorMessage('');
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="min-h-screen flex justify-center items-center bg-gradient-to-r from-blue-200 to-green-200">
+      <div className="bg-white p-6 rounded-md shadow-md w-full max-w-md">
+        <h1 className="text-4xl font-bold mb-6 text-center text-gray-700">
+          Currency Converter
+        </h1>
+        <div className="mb-6">
+          <input
+            type="text"
+            className="border rounded-md w-full p-3 focus:ring focus:ring-blue-300 border-blue-500 placeholder-gray-400 text-gray-800"
+            placeholder="Enter amount"
+            value={amount}
+            onChange={e => setAmount(e.target.value)}
+          />
+        </div>
+        <div className="flex items-center mb-6">
+          <Select
+            className="w-1/2"
+            options={currencyOptions.map(option => ({ value: option, label: option }))}
+            value={{ value: fromCurrency, label: fromCurrency }}
+            onChange={option => setFromCurrency(option.value)}
+            placeholder="Select currency"
+          />
+          <span className="text-gray-600 mx-4">to</span>
+          <Select
+            className="w-1/2"
+            options={currencyOptions.map(option => ({ value: option, label: option }))}
+            value={{ value: toCurrency, label: toCurrency }}
+            onChange={option => setToCurrency(option.value)}
+            placeholder="Select currency"
+          />
+        </div>
+        {errorMessage && (
+          <p className="text-red-600 mb-4 text-sm">{errorMessage}</p>
+        )}
+        <button
+          className="bg-blue-500 text-white py-3 px-6 rounded-md hover:bg-blue-600 transition-colors w-full"
+          onClick={handleConvert}
+        >
+          Convert
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        {convertedAmount && (
+          <p className="text-xl font-semibold mt-6">
+            {amount} {fromCurrency} = {convertedAmount} {toCurrency}
+          </p>
+        )}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
